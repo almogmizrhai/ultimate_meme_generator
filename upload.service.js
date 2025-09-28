4c90f320d32fb09fc078ac9bdb5e7ef58e28fd5f
Almog Mizrahi <almogosh111@gmail.com>
2025-09-28 17:20:52 +0300

    Share on Facebook page

--- upload.service.js
+++ upload.service.js
@@ -11,3 +11,22 @@ function loadFromStorage(key) {
     var data = localStorage.getItem(key)
     return data ? JSON.parse(data) : undefined
 }
+
+async function uploadImg(imgData, onSuccess) {
+    const CLOUD_NAME = 'webify'
+    const UPLOAD_URL = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`
+    const formData = new FormData()
+    formData.append('file', imgData)
+    formData.append('upload_preset', 'webify')
+    try {
+        const res = await fetch(UPLOAD_URL, {
+            method: 'POST',
+            body: formData
+        })
+        const data = await res.json()
+        onSuccess(data.secure_url)
+
+    } catch (err) {
+        console.log(err)
+    }
+}
